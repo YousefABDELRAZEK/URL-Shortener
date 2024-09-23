a30cb3d3af2bb5ed0c674fc3a2386c1c82fccaf3
Youssef SWE <155442169+YousefABDELRAZEK@users.noreply.github.com>
2024-09-23 18:10:16 +0300

    UrlController.php

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -8,17 +8,23 @@ use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
+    
     public function store(Request $request)
     {
+        //First we validate the user's input link
         $request->validate(['original_url' => 'required|url']);
+        //The str random can be any other value 
         $shortened = Str::random(6);
+        
         Url::create([
             'original_url' => $request->original_url,
             'shortened_url' => $shortened
         ]);
+        //return the success response to generate the shortened url
         return redirect()->back()->with('success', url($shortened));
     }
 
+    //function to handle the redirect method in the routes 
     public function redirect($shortened)
     {
         $url = Url::where('shortened_url', $shortened)->firstOrFail();
