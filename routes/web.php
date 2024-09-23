22d1e7c00ba9b436a3abca5e301ea90f96ad27c2
Youssef SWE <155442169+YousefABDELRAZEK@users.noreply.github.com>
2024-09-23 18:05:39 +0300

    web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    
-    return view('welcome');
-});
 
+//The main view of this app is the welcome view but i changed it's blade elemens.
+
+Route::get('/', function () {return view('welcome');});
+
+//Here the post and get routes are for the same view which is the welcome 
 
 Route::post('/shorten', [UrlController::class, 'store'])->name('shorten');
 Route::get('/{shortened}', [UrlController::class, 'redirect'])->name('redirect');
